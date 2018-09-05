19b4726c876592546c5ce7f7cdc6321d9f0b5f24
Sawyer McLane <samclane@gmail.com>
2018-09-05 13:34:39 -0600

    Added GPL. Removed requirements.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+"""
+    lifx-mycroft: Mycroft interaction for Lifx smart-lights
+    Copyright (C) 2018 Sawyer McLane
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
@@ -11,7 +29,6 @@ HUE, SATURATION, BRIGHTNESS, KELVIN = range(4)
 MAX_VALUE = 65535
 MAX_COLORTEMP = 9000
 MIN_COLORTEMP = 2500
-STEP_PERCENT = .25
 TRANSITION = 1250
 
 
